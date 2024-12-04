cefaf6820a87b6cf8856641084a09159a8dcc748
Jona <jonathang@hotmail.com.ar>
2024-12-03 23:56:57 -0300

    feat: add DropdownAvatar component with profile actions menu

---
+++ app/src/core/components/dropdown-avatar.component.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { Avatar } from "@nextui-org/avatar";
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/dropdown";
+
+export const DropdownAvatar = () => {
+  return (
+    <div className="flex items-center gap-4">
+      <Dropdown placement="bottom-end">
+        <DropdownTrigger>
+          <Avatar
+            isBordered
+            as="button"
+            className="transition-transform"
+            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+          />
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Profile Actions" variant="flat">
+          <DropdownItem key="profile" className="h-14 gap-2">
+            <p className="font-semibold">Sesión iniciada como</p>
+            <p className="font-semibold">noe@gmail.com</p>
+          </DropdownItem>
+          <DropdownItem key="settings">Mi perfil</DropdownItem>
+          <DropdownItem key="logout" color="danger">
+            Log Out
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  );
+};
