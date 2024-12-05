4bdf2f7927ff4cddd9efecae5a8923ea182a4193
Jona <jonathang@hotmail.com.ar>
2024-12-04 22:57:07 -0300

    feat: update color variants in primitives to include brand-specific options and adjust text sizes

--- components/primitives.ts
+++ components/primitives.ts
@@ -4,19 +4,13 @@ export const title = tv({
   base: "tracking-tight inline font-semibold",
   variants: {
     color: {
-      violet: "from-[#FF1CF7] to-[#b249f8]",
-      yellow: "from-[#FF705B] to-[#FFB457]",
-      blue: "from-[#5EA2EF] to-[#0072F5]",
-      cyan: "from-[#00b7fa] to-[#01cfea]",
-      green: "from-[#6FEE8D] to-[#17c964]",
-      pink: "from-[#FF72E1] to-[#F54C7A]",
-      foreground: "dark:from-[#FFFFFF] dark:to-[#4B4B4B]",
-      lightColor: "from-[#FFFFFF] to-[#E5E5E5]",
+      "brand-white": "#E6E6E6",
+      "brand-gradient-white-to-gray": "from-[#FFFFFF] to-[#E5E5E5]",
     },
     size: {
-      sm: "text-3xl lg:text-4xl",
-      md: "text-md",
-      lg: "text-[25px] font-bold",
+      sm: "text-[14px]",
+      md: "text-[20px]",
+      lg: "text-[25px]",
     },
     fullWidth: {
       true: "w-full block",
@@ -27,15 +21,7 @@ export const title = tv({
   },
   compoundVariants: [
     {
-      color: [
-        "violet",
-        "yellow",
-        "blue",
-        "cyan",
-        "green",
-        "pink",
-        "foreground",
-      ],
+      color: ["brand-white", "brand-gradient-white-to-gray"],
       class: "bg-clip-text text-transparent bg-gradient-to-b",
     },
   ],
