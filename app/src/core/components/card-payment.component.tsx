4fe0e76bad2f5492bfc41b7cc1f9288b08891412
Jona <jonathang@hotmail.com.ar>
2024-12-05 22:21:53 -0300

    feat: add payment card

---
+++ app/src/core/components/card-payment.component.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+import netflix from "@/assets/netflix.png";
+import { title } from "@/components/primitives";
+
+export const CardPayment = () => {
+  return (
+    <article className="flex justify-between p-4 rounded-2xl bg-brand-gradient-black-transparent">
+      <div className="flex gap-4 items-center">
+        <Image src={netflix.src} alt="Netflix" width={49} height={49} />
+        <div>
+          <h2 className="font-light">Netflix</h2>
+          <p className="text-brand-white text-[10px]">Stream</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-start">
+        <h3
+          className={`${title({
+            color: "brand-white",
+            size: "md",
+            className: "font-bold",
+          })}`}
+        >
+          $320.000
+        </h3>
+        <p className="text-brand-light-green text-[10px]">Martes 10 Nov</p>
+      </div>
+    </article>
+  );
+};
