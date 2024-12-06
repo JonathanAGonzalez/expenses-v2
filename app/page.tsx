0ffc2c95cf0e43c5b87c141254b3d2e485086ee0
Jona <jonathang@hotmail.com.ar>
2024-12-06 13:28:17 -0300

    refactor:  add components dynamics

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,16 @@
-import { CardPayment } from "./src/core/components/card-payment.component";
-import { CardWithBorder } from "./src/core/components/card-with-border.component";
-import { SubTitle } from "./src/core/components/sub-title.component";
-import { HomeHeaderTitleCategory } from "./src/features/home/components/home-header-title-category.component";
 import { HomeWidget } from "./src/features/home/components/home-widget.component";
 import { Navbar } from "./src/core/components/nav-bar";
+import { HomeSectionCategory } from "./src/features/home/components/home-section-category.component";
+import { HomeSectionLastAdded } from "./src/features/home/components/home-section-last-added.component";
+import { categoriesDefault } from "./src/core/categories-default";
 
 export default function Home() {
   return (
     <>
       <section>
         <HomeWidget />
-        <HomeHeaderTitleCategory />
-        <section className="grid grid-cols-2 gap-y-2 gap-x-5 pt-2 [@media(max-width:383px)]:grid-cols-1">
-          <CardWithBorder />
-          <CardWithBorder />
-          <CardWithBorder />
-          <CardWithBorder />
-        </section>
-        <section className="pt-8">
-          <SubTitle>Ultimos agregados</SubTitle>
-          <div className="grid gap-3 pt-2 pb-[95px]">
-            <CardPayment />
-            <CardPayment />
-            <CardPayment />
-            <CardPayment />
-            <CardPayment />
-            <CardPayment />
-            <CardPayment />
-          </div>
-        </section>
+        <HomeSectionCategory items={categoriesDefault} />
+        <HomeSectionLastAdded />
       </section>
       <Navbar />
     </>
